189442b222da332480746e31df92028de11d43fa
Hamed Mirzaei <HamedMirzaei2001Official@gmail.com>
2024-07-20 11:38:03 +0330

    local storage

---
+++ local_storage/main.js
@@ -0,0 +1,11 @@
+// key value database.
+
+window.localStorage.setItem('color', 'red');
+console.log(window.localStorage.getItem('color'));
+
+// clear an item:
+// window.localStorage.removeItem('color');
+
+// clear all storage:
+// window.localStorage.clear()
+
